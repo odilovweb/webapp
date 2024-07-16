3558d48b97825ac2fde8ab617c399bbd4ef0b187
odilovweb <mirzohidodilov49@gmail.com>
2024-07-16 22:43:32 +0500

    wverv

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -159,7 +159,7 @@ function Home(props) {
         <nav className="bg-slate-600 rounded-xl px-4 py-5 ">
           <div className="flex container  items-center justify-between mb-3">
             {userData && userData.tickets > 0 ? (
-              <Link to="/miningv" className="btn btn-sm  btn-warning">
+              <Link to="/mining" className="btn btn-sm  btn-warning">
                 Play for 1 Ticket
               </Link>
             ) : (
