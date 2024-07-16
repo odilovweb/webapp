ce1576c4a2b298711630c0f2c8ddc6b9708899c8
odilovweb <mirzohidodilov49@gmail.com>
2024-07-16 12:37:35 +0500

    done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -135,10 +135,10 @@ function Home(props) {
             alt="Image"
             className="w-56 h-56 mx-auto rounded-full"
           />
-          <h3 className="font-bold mx-auto max-w-12 w-full">
-            {userData && tgName.slice(0, 10)}
-          </h3>
-        </div>
+        </div>{" "}
+        <h3 className="font-bold mx-auto max-w-12 w-full">
+          {userData && tgName.slice(20)}
+        </h3>
         <nav className="bg-slate-600 rounded-xl px-4 py-5 ">
           <div className="flex container  items-center justify-between mb-3">
             <Link to="mining" className="btn btn-sm  btn-warning">
