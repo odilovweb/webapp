9b43d55b9a7fa9a309be8c05cab4b4b3f2aacd0f
odilovweb <mirzohidodilov49@gmail.com>
2024-07-16 11:47:35 +0500

    done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -46,7 +46,7 @@ function Home(props) {
       const user = telegram.initDataUnsafe.user;
       getUserData(user.id);
       setTGId(user.id);
-      setTgPhoto(user.photo);
+      setTgPhoto(user.photoUrl());
     }
 
     console.log(userData);
