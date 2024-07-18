50b21f47ee9177a71788d164ad49f0d8270f6214
odilovweb <mirzohidodilov49@gmail.com>
2024-07-18 20:08:20 +0500

    dew

--- src/pages/Friends.jsx
+++ src/pages/Friends.jsx
@@ -32,7 +32,7 @@ function Friends() {
   useEffect(() => {
     telegram.ready();
     if (telegram.initDataUnsafe) {
-      getUserData(telegram.initDataUnsafe.user.id);
+      getUserData("841886966");
     }
   }, []);
 
